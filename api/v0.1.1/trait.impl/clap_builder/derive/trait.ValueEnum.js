bc037cdaaa79089bf7afe9420ed3687972ffb005
jlotthammer <jlotthammer@users.noreply.github.com>
2026-04-21 19:44:23 +0000

    deploy: b1388399eab10ec9ada34363f94ddd9f47a7e765

---
+++ api/v0.1.1/trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["rfasta",[["impl ValueEnum for <a class=\"enum\" href=\"rfasta/clean/enum.DuplicateAction.html\" title=\"enum rfasta::clean::DuplicateAction\">DuplicateAction</a>",0],["impl ValueEnum for <a class=\"enum\" href=\"rfasta/clean/enum.InvalidSequenceAction.html\" title=\"enum rfasta::clean::InvalidSequenceAction\">InvalidSequenceAction</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[344]}
